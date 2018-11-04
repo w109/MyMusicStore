a4e11fba0e98d19d0da037a81fad08f495861531
w109 <1094891961@qq.com>
2018-11-04 12:30:16 +0800

    44

---
+++ wfj/Demo/Demo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用数据上下文进行数据操作，using不是上下文代码的范围，执行完后内存就会自动释放
+            using (var context = new CoursoDBEntities())
+            {
+                var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Nane, d.Dscn);
+                Console.ReadKey();
+            }
+        }
+    }
+}
