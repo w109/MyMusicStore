53c3d37e7eb0b99c7a92f6631cf6ba284241cf20
w109 <1094891961@qq.com>
2018-11-05 15:54:11 +0800

    更新实体

--- wfj/Demo/Demo/Program.cs
+++ wfj/Demo/Demo/Program.cs
@@ -16,6 +16,27 @@ namespace Demo
                 var departments = context.Departments.OrderBy(n => n.SortCode).ToList();
                 foreach (var d in departments)
                     Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Nane, d.Dscn);
+
+                Console.WriteLine("添加一条新的部门记录");
+                //添加一条记录
+                var newDepartment = new Departments
+                {
+                    ID = Guid.NewGuid(),
+                    Nane = "财务学院",
+                    Dscn = "财经与物流",
+                    SortCode="006"
+                };
+                //把新的对象添加到上下文中
+                context.Departments.Add(newDepartment);
+                //更新上下文，把新的实体保存到数据库中
+                context.SaveChanges();
+
+                //显示新的记录
+                var departments1 = context.Departments.OrderBy(n => n.SortCode).ToList();
+                foreach (var d in departments1)
+                    Console.WriteLine("编号{0},部门名称{1},说明{2}", d.SortCode, d.Nane, d.Dscn);
+
+
                 Console.ReadKey();
             }
         }
