1aa243d48a7df8078c859ac5ddf3e66b5c31dfc4
w109 <1094891961@qq.com>
2018-11-08 11:16:01 +0800

    练习

---
+++ wfj/C#/CodeFirstDemo/CodeFirstDemo/CodeFirstModels/Course.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.CodeFirstModels
+{
+    /// <summary>
+    /// 课程实体
+    /// </summary>
+    public class Course
+    {
+        public Guid ID { get; set; }
+       
+        public string Title { get; set; }
+        
+        public int Credit { get; set; } = 1;
+        public string CourseCode { get; set; }
+
+        public int StudyPeriod { get; set; } = 0;
+        
+        public virtual Department Department { get; set; }
+
+        public Course()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
---
+++ wfj/C#/CodeFirstDemo/CodeFirstDemo/CodeFirstModels/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstDemo.CodeFirstModels
+{
+    /// <summary>
+    /// 二级学院的实体
+    /// </summary>
+    public class Department
+    {
+       
+        public Guid ID { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Description { get; set; }
+        
+        public string SortCode { get; set; }
+
+        public Department()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+}
