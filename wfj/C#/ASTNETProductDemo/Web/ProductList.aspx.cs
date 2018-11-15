ff92eb03653829db3033f84ceb8609de5f2918e4
w109 <1094891961@qq.com>
2018-11-15 11:06:20 +0800

    删除操作

--- wfj/C#/ASTNETProductDemo/Web/ProductList.aspx.cs
+++ wfj/C#/ASTNETProductDemo/Web/ProductList.aspx.cs
@@ -24,4 +24,23 @@ public partial class ProductList : System.Web.UI.Page
             GridView1.DataBind();
         }
     }
+
+    //翻页事件
+    protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        GridView1.PageIndex = e.NewPageIndex;
+        _getData();
+    }
+
+    protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        var id = Guid.Parse(GridView1.DataKeys[e.RowIndex].Value.ToString());
+        using (var context = new ProductDbContext())
+        {
+            var delproduct = context.Products.Find(id);
+            context.Products.Remove(delproduct);
+            context.SaveChanges();
+                }
+        _getData();
+        }
 }
