c3415eca0d2378ae2c2e1ce9a1a89e4825c715b1
w109 <1094891961@qq.com>
2018-11-26 18:12:33 +0800

    1

---
+++ wfj/C#/MVC5Demo/MvcDemo1/MvcDemo1/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcDemo1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            //return View("../../Views/Test/test1");
+            return View();
+        }
+
+        public string Welcome(string name,int id=1)
+        {
+            return "您好,"+name+",欢迎次数："+id;
+        }
+    }
+}
