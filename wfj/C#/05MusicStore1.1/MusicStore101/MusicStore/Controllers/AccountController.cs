3dc0ac528f438c37e60b413ae60d9566c91e55d9
w109 <1094891961@qq.com>
2018-12-05 15:34:30 +0800

    注册的视图模型

--- wfj/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
+++ wfj/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/AccountController.cs
@@ -13,9 +13,20 @@ namespace MusicStore.Controllers
 {
     public class AccountController : Controller
     {
-        // GET: Account
+        /// <summary>
+        /// 填写注册信息
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            //用户的保存 Person ApplicationUser
             return View();
         }
 
@@ -35,6 +46,7 @@ namespace MusicStore.Controllers
         }
 
         [HttpPost]   //此Action用来接收用户提交
+        [ValidateAntiForgeryToken]
         public ActionResult Login(LoginViewModel model, string returnUrl)
         {
             //判断实体是否校验通过
