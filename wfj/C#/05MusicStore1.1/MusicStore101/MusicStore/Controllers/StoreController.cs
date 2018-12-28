aebf5f8e1324f82b088b6aa24a442a20d724e3a9
w109 <1094891961@qq.com>
2018-12-28 08:43:22 +0800

    点击评论的回复，把原回复内容赋值到百度编辑器，把ID保存到隐藏控件

--- wfj/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
+++ wfj/C#/05MusicStore1.1/MusicStore101/MusicStore/Controllers/StoreController.cs
@@ -33,24 +33,26 @@ namespace MusicStore.Controllers
         /// <returns></returns>
         private string _GetHtml(List<Reply> cmt)
         {
-            var htmlString = "";
-            htmlString += "<ul class='media-list'>";
-            foreach (var item in cmt)
-            {
-                htmlString += "<li class='media'>";
-                htmlString += "<div class='media-left'>";
-                htmlString += "<img class='media-object' src='" + item.Person.Avarda +
-                              "' alt='头像' style='width:40px;border-radius:50%;'>";
-                htmlString += "</div>";
-                htmlString += "<div class='media-body'>";
-                htmlString += "<h5 class='media-heading'>" + item.Person.Name + "  发表于" +
-                              item.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + "</h5>";
-                htmlString += item.Content;
-                //查询当前回复的下一级回复
-                var sonCmt = _context.Replies.Where(x => x.ParentReply.ID == item.ID).ToList();
-                htmlString += "<h6>回复(<a href='#'>" + sonCmt.Count + "</a>)条" + "  <a href='#'><i class='glyphicon glyphicon-thumbs-up'></i></a>(" +
-                              item.Like + ")  <a href='#'><i class='glyphicon glyphicon-thumbs-down'></i></a>(" + item.Hate + ")</h6>";
-                htmlString += "</div>";
+                var htmlString = "";
+                htmlString += "<ul class='media-list'>";
+                foreach (var item in cmt)
+                {
+                    htmlString += "<li class='media'>";
+                    htmlString += "<div class='media-left'>";
+                    htmlString += "<img class='media-object' src='" + item.Person.Avarda +
+                                  "' alt='头像' style='width:40px;border-radius:50%;'>";
+                    htmlString += "</div>";
+                    htmlString += "<div class='media-body' id='Content-" + item.ID + "'>";
+                    htmlString += "<h5 class='media-heading'>" + item.Person.Name + "  发表于" +
+                                  item.CreateDateTime.ToString("yyyy年MM月dd日 hh点mm分ss秒") + "</h5>";
+                    htmlString += item.Content;
+                    htmlString += "</div>";
+                    //查询当前回复的下一级回复
+                    var sonCmt = _context.Replies.Where(x => x.ParentReply.ID == item.ID).ToList();
+                htmlString += "<h6><a href='#div-editor' class='reply' onclick=\"javascript:GetQuote('" + item.ID + "');\">回复</a>(<a href='#' class='reply'>" + sonCmt.Count + "</a>)条" +
+                              "<a href='#' class='reply' style='margin:0 20px 0 40px'><i class='glyphicon glyphicon-thumbs-up'></i>(" +
+                              item.Like + ")</a><a href='#' class='reply' style='margin:0 20px'><i class='glyphicon glyphicon-thumbs-down'></i>(" + item.Hate + ")</a></h6>";
+
                 htmlString += "</li>";
             }
             htmlString += "</ul>";
